10999b4e741fd3cf62f0b656c6ed981c87ca3cd8
Akkel7 <luc7agbeze@gmail.com>
2025-10-24 19:03:50 +0530

    Add files via upload
    My first codes in C.

---
+++ Lab_1/intro.c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("\nNOM             : KING KODZO EDEM LUC AGBEZE");
+	printf("\nROLL NUMBER     : MEB25039");
+	printf("\nDEPARTMENT      : Mechanical Engineering");
+	printf("\nESTABLISHMENT   : Tezpur University");
+	
+	
+	
+	
+	return 0;
+}
