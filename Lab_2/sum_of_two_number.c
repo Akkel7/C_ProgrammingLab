e4758124571c5e40ad7470e8088853dd99646c71
Akkel7 <luc7agbeze@gmail.com>
2025-11-21 22:20:21 +0530

    Update sum_of_two_number.c
    I add title and comments.

--- Lab_2/sum_of_two_number.c
+++ Lab_2/sum_of_two_number.c
@@ -3,13 +3,14 @@
 int main()
 {
    float numb1, numb2, sum;
-   printf("ENTER TWO NUMBERS: ");
-   printf("\nNUMBER 1: ");
-   scanf("%f",&numb1);
-   printf("\nNUMBER 2: ");
-   scanf("%f",&numb2);
+   printf("======CALCULATION OF THE SUM OF TWO NUMBERS=======")
+   printf("\n\nENTER TWO NUMBERS: ");
+   printf("\nNUMBER 1: ");   
+   scanf("%f",&numb1);      //User input number 1
+   printf("\nNUMBER 2: ");  
+   scanf("%f",&numb2);      // User input number 2
 
    sum = numb1 + numb2;
-   printf("THE SUM OF %f AND %f IS: %f",numb1, numb2, sum);
+   printf("THE SUM OF %f AND %f IS: %f",numb1, numb2, sum); // Output of the sum of number 1 and number 2
     return 0;
 }
